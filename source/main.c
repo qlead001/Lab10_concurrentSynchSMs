47ddda21c52c596da65b394c4564a2caf0c830c8
qlead001 <qlead001@ucr.edu>
2020-05-18 09:36:41 -0700

    Initialise repository with lab files

---
+++ source/main.c
@@ -0,0 +1,24 @@
+/*	Author: Quinn Leader qlead001@ucr.edu
+ *  Partner(s) Name: NA
+ *	Lab Section: 026
+ *	Assignment: Lab 10  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include "timer.h"
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
